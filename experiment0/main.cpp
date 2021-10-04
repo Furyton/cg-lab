124607fb140d4c7d27dfdf4ef38685ad1cf0632d
Furyton <1543159426@qq.com>
2021-10-04 22:52:22 +0800

    processInput

--- experiment0/main.cpp
+++ experiment0/main.cpp
@@ -7,6 +7,20 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
 int main() {
     a = 1;
     glfwInit();
@@ -34,6 +48,11 @@ int main() {
 
     while(!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
